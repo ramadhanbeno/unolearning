cdb30ff2c78e62d132a94e4a339e256b57a9eec9
benoramadhan <010100000101y3s@gmail.com>
2019-11-18 14:09:20 +0700

    first

---
+++ application/views/home/index.php
@@ -0,0 +1,57 @@
+<div class="container">
+  <nav class="navbar navbar-expand-lg">
+
+    <a class="navbar-brand" href="#">WebUno</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="navbar-nav ml-auto" style="font-family: Viga">
+        <a class="nav-item nav-link" style="text-transform: capitalize;" href="<?= base_url(); ?>">Dashboard <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" style="text-transform:capitalize" href="#">Komponen</a>
+        <a class="nav-item nav-link" style="text-transform:capitalize" href="#">MyDemo</a>
+      </div>
+    </div>
+  </nav>
+</div>
+
+<div class="container arduino">
+  <div class="unoinfo">
+    <div class="row justify-content-center">
+      <div class="col">
+        <img src="<?= base_url(); ?>assets/images/uno.png">
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- content2 -->
+<div class="row pt-2 mt-4">
+  <div class="col-sm">
+    <div class="card">
+      <div class="card-header text-center">
+        Komponen
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-sm">
+            <h5>Komponen 1</h5>
+            <p>With supporting text below as a natural lead-in to additional content.</p>
+          </div>
+          <div class="col-sm">
+            <h5>Komponen 3</h5>
+            <p>With supporting text below as a natural lead-in to additional content.</p>
+          </div>
+          <div class="col-sm">
+            <h5>Komponen 3</h5>
+            <p>With supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="card-footer text-muted">
+        2 days ago
+      </div>
+    </div>
+  </div>
+</div>
